c7830dc665f38f69f521c31f717bd26ee4c1beaa
ernie <simutisernestas@gmail.com>
2023-12-08 09:52:04 +0100

    day5 part2
    in reasonable time : )

--- day5/part2.cpp
+++ day5/part2.cpp
@@ -5,6 +5,7 @@
 #include <array>
 #include <iostream>
 #include <limits>
+#include <algorithm>
 
 void space_sep_string_to_vector(const std::string str, std::vector<long> &vec) {
     std::string num_str;
@@ -24,8 +25,6 @@ int main() {
     std::string input_str;
     // parse from file input.txt
     std::ifstream input_file("../day5/input.txt");
-
-
 
     // parse line by line
     std::string line;
@@ -61,39 +60,51 @@ int main() {
                                               "light-to-temperature",
                                               "temperature-to-humidity",
                                               "humidity-to-location"};
-    for (size_t i = 0; i < seeds.size() - 1; i += 2) {
-        auto s1 = seeds[i];
-        auto s2 = seeds[i + 1];
+    std::reverse(order_of_iter.begin(), order_of_iter.end());
+    long location = 0;
+    uint8_t found = 0;
+    long step = 100000;
+    while (true) {
+        long pre_entity = location;
+        for (const auto &map: order_of_iter) {
+            long an_entity = 0;
+            // map seed to soil
+            for (const auto &range: maps[map]) {
+                auto destination_range_start = range[1];
+                auto source_range_start = range[0];
+                auto distance = range[2];
 
-        for (long seed = s1; seed <= s1 + s2; ++seed) {
-            long pre_entity = seed;
-            for (const auto &map: order_of_iter) {
-                long an_entity = 0;
-                // map seed to soil
-                for (const auto &range: maps[map]) {
-                    auto destination_range_start = range[0];
-                    auto source_range_start = range[1];
-                    auto distance = range[2];
-
-                    if (pre_entity >= source_range_start && pre_entity < source_range_start + distance) {
-                        long diff = (destination_range_start - source_range_start);
-                        an_entity = pre_entity + diff;
-                        break;
-                    } else {
-                        an_entity = pre_entity;
-                    }
+                if (pre_entity >= source_range_start && pre_entity < source_range_start + distance) {
+                    long diff = (destination_range_start - source_range_start);
+                    an_entity = pre_entity + diff;
+                    break;
+                } else {
+                    an_entity = pre_entity;
                 }
-                pre_entity = an_entity;
             }
+            pre_entity = an_entity;
+        }
 
-            if (pre_entity < lowest_location) {
-                lowest_location = pre_entity;
+        // check if it falls in the range of seeds
+        for (size_t i = 0; i < seeds.size() - 1; i += 2) {
+            auto s1 = seeds[i];
+            auto s2 = seeds[i + 1];
+            if (pre_entity >= s1 && pre_entity <= s1 + s2) {
+                found++;
             }
         }
 
-        std::cout << "iter" << i << "\n";
+        if (found == 1 && step != 1) {
+            std::cout << "found 1: " << location << "\n";
+            location -= step;
+            step = 1;
+        } else if (found == 2) {
+            break;
+        }
+
+        location += step;
     }
 
-    std::cout << "\nlocation: " << lowest_location << "\n";
+    std::cout << "location: " << location << "\n";
     return 0;
 }
